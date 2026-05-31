098aff32d826084fb864028fe8943c6de87203f2
Rebekah Adams <rebekah.adams42@outlook.com>
2026-05-30 23:12:16 -0400

    Update krpc_basic_launch.py

--- krpc_basic_launch.py
+++ krpc_basic_launch.py
@@ -4,8 +4,8 @@ import math
 # connection to the server 
 conn = krpc.connect(
     name='Launch Sequence',
-    address='192.168.1.138',
-    rpc_port=50000, stream_port=50001)
+    address='',
+    rpc_port=xxx, stream_port=xxx)
 print(conn.krpc.get_status().version)
 
 # Get the active vessel
